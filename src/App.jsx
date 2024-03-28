bac72ca17230568394202d90edc71db4f2054d02
Kosta45 <konstanlebed8@gmail.com>
2024-03-28 22:01:42 +0100

    Work with hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[6.25rem]">
         <Header />
+        <Hero />
       </div>
+
       <ButtonGradient />
     </>
   );
