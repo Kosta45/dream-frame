74d264440586472abbe3f83353c0eca155804f00
Kosta45 <konstanlebed8@gmail.com>
2024-04-03 11:36:35 +0200

    generationg component

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { loading } from "../assets";
 
 const Generating = () => {
   return (
     <div>
-      <img className="w-5 h-5 mr-4" />
+      <img className="w-5 h-5 mr-4" src={loading} />
     </div>
   );
 };
