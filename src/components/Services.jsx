96584af6271452faadcd183b9144d5fdb84199ff
Kosta45 <konstanlebed8@gmail.com>
2024-04-11 00:26:41 +0200

    services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+
 import Section from "./Section";
 import Heading from "./Heading";
+
+import { service1, service2, service3, check } from "../assets";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 
 const Services = () => {
   return (
@@ -10,6 +14,45 @@ const Services = () => {
           title="Generative AI made for creators."
           text="Dreamframe unlocks the potential of AI-powered applications"
         />
+
+        <div className="relative">
+          <div
+            className="relative z-1 flex items-centerh-[39rem] 
+          mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]"
+          >
+            <div
+              className="absolute top-0 left-0 w-full h-full
+            pointer-events-none md:w-3/5 xl:w-auto"
+            >
+              <img
+                className="w-full h-full object-cover md:object-right"
+                width={800}
+                alt="Smartes tAI"
+                height={730}
+                src={service1}
+              />
+            </div>
+
+            <div className="relative z-1 max-[17rem] ml-auto">
+              <h4 className="h-4 mb-4">Smartest AI</h4>
+              <p className="body-2 mb-[3rem] text-n-3">
+                Dreamframe unlocks the potential of AI-powered applications
+              </p>
+              <ul className="body-2">
+                {brainwaveServices.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t
+                  border-n-6"
+                  >
+                    <img width={24} height={24} src={check} alt="" />
+                    <p className="ml-4">{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </Section>
   );
